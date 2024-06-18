811f32ffe3a69a795d8390110cacb1002c244bf6
Fabio Amorelli <amorelli.ff@gmail.com>
2024-06-18 12:53:57 -0300

    few adjustments, not optimally working

--- src/controllers/disciplina.controller.ts
+++ src/controllers/disciplina.controller.ts
@@ -1,6 +1,41 @@
 import { Request, Response } from 'express';
 import { Disciplina, DisciplinaInput } from '../models/disciplina.model';
 
+const updateDisciplina = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { cargaHoraria, coRequisito, codDisciplina, ementa, nome, preRequisitos } = req.body;
+
+  const disciplina = await Disciplina.findOne({ _id: id });
+
+  if (!disciplina) {
+    return res.status(404).json({ message: `Disciplina with id "${id} not found.` });
+  }
+
+  if (!codDisciplina || !nome || !cargaHoraria || !ementa) {
+    return res.status(422).json({
+      message: 'The fields codDisciplina, nome, cargaHoraria, and ementa are required',
+    });
+  }
+
+  await Disciplina.updateOne({ _id: id }, { codDisciplina, nome, cargaHoraria, ementa, preRequisitos, coRequisito });
+
+  const disciplinaUpdated = await Disciplina.findById(id);
+
+  return res.status(200).json({ data: disciplinaUpdated });
+};
+
+const findByName = async (req: Request, res: Response) => {
+  const { codDisciplina } = req.body;
+
+  const disciplina = await Disciplina.findOne({ codDisciplina });
+
+  if (!disciplina) {
+    return res.status(404).json({ message: `Disciplina with name "${codDisciplina}" not found.` });
+  }
+
+  return res.status(200).json({ data: disciplina });
+};
+
 const createDisciplina = async (req: Request, res: Response) => {
   const { cargaHoraria, coRequisito, codDisciplina, ementa, nome, preRequisitos } = req.body;
 
@@ -10,14 +45,27 @@ const createDisciplina = async (req: Request, res: Response) => {
     });
   }
 
+  const searchDisciplina = await Disciplina.findOne({ codDisciplina });
+
+  if (searchDisciplina) {
+    return updateDisciplina(req, res);
+    return res.status(200).json({ data: searchDisciplina });
+  }
+
   const disciplinaInput: DisciplinaInput = {
     codDisciplina,
     nome,
     cargaHoraria,
     ementa,
-    preRequisitos,
-    coRequisito,
   };
+
+  if (coRequisito) {
+    disciplinaInput.coRequisito = coRequisito;
+  }
+
+  if (preRequisitos) {
+    disciplinaInput.preRequisitos = preRequisitos;
+  }
 
   const disciplinaCreated = await Disciplina.create(disciplinaInput);
 
@@ -42,47 +90,12 @@ const getDisciplina = async (req: Request, res: Response) => {
   return res.status(200).json({ data: disciplina });
 };
 
-const updateDisciplina = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { cargaHoraria, coRequisito, codDisciplina, ementa, nome, preRequisitos } = req.body;
-
-  const disciplina = await Disciplina.findOne({ _id: id });
-
-  if (!disciplina) {
-    return res.status(404).json({ message: `Disciplina with id "${id} not found.` });
-  }
-
-  if (!codDisciplina || !nome || !cargaHoraria || !ementa) {
-    return res.status(422).json({
-      message: 'The fields codDisciplina, nome, cargaHoraria, and ementa are required',
-    });
-  }
-
-  await Disciplina.updateOne({ _id: id }, { codDisciplina, nome, cargaHoraria, ementa, preRequisitos, coRequisito });
-
-  const disciplinaUpdated = await Disciplina.findById(id);
-
-  return res.status(200).json({ data: disciplinaUpdated });
-};
-
 const deleteDisciplina = async (req: Request, res: Response) => {
   const { id } = req.params;
 
   await Disciplina.findByIdAndDelete(id);
 
   return res.status(200).json({ data: 'Disciplina deleted' });
-};
-
-const findByName = async (req: Request, res: Response) => {
-  const { codDisciplina } = req.body;
-
-  const disciplina = await Disciplina.findOne({ codDisciplina });
-
-  if (!disciplina) {
-    return res.status(404).json({ message: `Disciplina with name "${codDisciplina}" not found.` });
-  }
-
-  return res.status(200).json({ data: disciplina });
 };
 
 export { createDisciplina, getAllDisciplinas, getDisciplina, updateDisciplina, deleteDisciplina, findByName };
--- src/controllers/periodo.controller.ts
+++ src/controllers/periodo.controller.ts
@@ -1,6 +1,29 @@
 import { Request, Response } from 'express';
 import { Periodo, PeriodoInput } from '../models/periodo.model';
 
+const updatePeriodo = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { idPeriodo, periodo } = req.body;
+
+  const periodoObj = await Periodo.findOne({ _id: id });
+
+  if (!periodoObj) {
+    return res.status(404).json({ message: `Periodo with id "${id} not found.` });
+  }
+
+  if (!periodo || !idPeriodo) {
+    return res.status(422).json({
+      message: 'The fields periodo and idPeriodo are required',
+    });
+  }
+
+  await Periodo.updateOne({ _id: id }, { idPeriodo, periodo });
+
+  const periodoUpdated = await Periodo.findById(id);
+
+  return res.status(200).json({ data: periodoUpdated });
+};
+
 const createPeriodo = async (req: Request, res: Response) => {
   const { idPeriodo, periodo } = req.body;
 
@@ -8,6 +31,13 @@ const createPeriodo = async (req: Request, res: Response) => {
     return res.status(422).json({
       message: 'The fields periodo and idPeriodo are required',
     });
+  }
+
+  const searchPeriodo = await Periodo.findOne({ idPeriodo });
+
+  if (searchPeriodo) {
+    return updatePeriodo(req, res);
+    return res.status(200).json({ data: searchPeriodo });
   }
 
   const periodoInput: PeriodoInput = {
@@ -37,29 +67,6 @@ const getPeriodo = async (req: Request, res: Response) => {
   }
 
   return res.status(200).json({ data: periodo });
-};
-
-const updatePeriodo = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { idPeriodo, periodo } = req.body;
-
-  const periodoObj = await Periodo.findOne({ _id: id });
-
-  if (!periodoObj) {
-    return res.status(404).json({ message: `Periodo with id "${id} not found.` });
-  }
-
-  if (!periodo || !idPeriodo) {
-    return res.status(422).json({
-      message: 'The fields periodo and idPeriodo are required',
-    });
-  }
-
-  await Periodo.updateOne({ _id: id }, { idPeriodo, periodo });
-
-  const periodoUpdated = await Periodo.findById(id);
-
-  return res.status(200).json({ data: periodoUpdated });
 };
 
 const deletePeriodo = async (req: Request, res: Response) => {
